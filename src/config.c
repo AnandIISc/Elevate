6c58acd168cd79c90efd9e9fff10c010ace2126c
AnandIISc <aanand.4kr@gmail.com>
2026-06-07 21:34:01 +0530

    src/config : Implemented differntiat base name command vs absolute command

--- src/config.c
+++ src/config.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+char* get_command_basename(char* cmd){
+    char* base = strrchr(cmd, '/');
+    if(base == NULL){
+        return cmd;
+    }
+    return base + 1;
+}
+
 int is_allowed(char *user, char *cmd){
     // TODO: Implement this function
 
@@ -11,6 +19,7 @@ int is_allowed(char *user, char *cmd){
         return 0;
     }
     
+    char *resolved_command = get_command_basename(cmd);
 
     char cfg_name[100];
     char cfg_operation[100];
@@ -18,7 +27,7 @@ int is_allowed(char *user, char *cmd){
     while(fscanf(fp, "%99s %99s", cfg_name, cfg_operation) == 2){
         if(strcmp(cfg_name, user) == 0 ){
 
-            if (strcmp(cfg_operation, cmd) == 0 || strcmp(cfg_operation, "*") == 0){
+            if (strcmp(cfg_operation, resolved_command) == 0 || strcmp(cfg_operation, "*") == 0){
                 fclose(fp);
                 return 1;
             }
